24a05aa5505d3ea9b3dad153691aaf8271fc1f19
Isael Feliciano <rogelioisael@hotmail.com>
2017-06-20 17:15:17 -0400

    FIX Partial payment
    Was saving the amount entered not the amount remaining

--- app.js
+++ app.js
@@ -480,7 +480,7 @@ function saveReceipt(print) {
 
 	if (amount < addPaymentPage.tempReceiptAmount) {
 		status = 'Sin pagar'
-		amountForSaveInInvoice = addPaymentPage.receiptAmount;
+		amountForSaveInInvoice = addPaymentPage.tempReceiptAmount - addPaymentPage.receiptAmount;
 	} else if ((amount - addPaymentPage.tempReceiptAmount) === 0) {
 		status = 'Pagada';
 		amount = addPaymentPage.tempReceiptAmount;
